47f150258c8ba841ad339243c78a9143ba57a9f6
lesanpi <lespinerua@gmail.com>
2019-02-25 15:12:46 -0400

    Updates

---
+++ precache-manifest.1ed0055c01b952ade040887f925ec0c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c06f5e5a2ae47006c0b77be4de6a2077",
+    "url": "/platzivideo/static/media/logo.c06f5e5a.png"
+  },
+  {
+    "revision": "d86d17d420a790d0221a",
+    "url": "/platzivideo/static/js/runtime~main.d86d17d4.js"
+  },
+  {
+    "revision": "7f133e4ca988f46c42d5",
+    "url": "/platzivideo/static/js/main.7f133e4c.chunk.js"
+  },
+  {
+    "revision": "3ad84101692611e66582",
+    "url": "/platzivideo/static/js/2.3ad84101.chunk.js"
+  },
+  {
+    "revision": "7f133e4ca988f46c42d5",
+    "url": "/platzivideo/static/css/main.46934690.chunk.css"
+  },
+  {
+    "revision": "a1f114817211c56816eaf273149032d8",
+    "url": "/platzivideo/index.html"
+  }
+];
